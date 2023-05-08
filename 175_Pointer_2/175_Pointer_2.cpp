8165eb297f3244dd22133ca685413618e1776038
NaufalSatriaa <satriana0017@gmail.com>
2023-05-08 15:40:51 +0700

    push git

--- 175_Pointer_2/175_Pointer_2.cpp
+++ 175_Pointer_2/175_Pointer_2.cpp
@@ -1,20 +1,45 @@
-// 175_Pointer_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class siswa;
+
+class orang {
+private:
+	string nama;
+public:
+	void setnama(string pnama);
+	friend class siswa;
+};
+
+class siswa {
+private:
+	int id;
+public:
+	void setid(int pid);
+	void displayall(orang& a);
+};
+
+void siswa::displayall(orang& a) {
+	cout << id << endl << a.nama;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void siswa::setid(int pid) {
+	id = pid;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+
+void orang::setnama(string pnama) {
+	nama = pnama;
+}
+
+int main() {
+	orang o;
+	o.setnama("joko kumat");
+	siswa s;
+	s.setid(1);
+	s.displayall(o);
+	cout << endl;
+	system("pause");
+	return 0;
+}
